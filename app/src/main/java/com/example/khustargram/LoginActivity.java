78da0f0a4bc8deef5ebb140fd4548b833f364a3e
ChoiByeol <honey3388@khu.ac.kr>
2021-12-11 05:39:11 +0900

    feat: 파이어베이스 연동, 회원가입 구현

--- app/src/main/java/com/example/khustargram/LoginActivity.java
+++ app/src/main/java/com/example/khustargram/LoginActivity.java
@@ -1,14 +1,118 @@
 package com.example.khustargram;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    private final String TAG = LoginActivity.class.getSimpleName();
+
+    private FirebaseAuth auth;
+
+    // email sign in
+    private EditText email;
+    private EditText password;
+    private Button emailLoginButton;
+
+    // login listener
+    FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
+
+        email = (EditText) findViewById(R.id.email_edittext);
+        password = (EditText) findViewById(R.id.password_edittext);
+        emailLoginButton = (Button) findViewById(R.id.email_login_button);
+        emailLoginButton.setOnClickListener(this::onClick);
+
+        // login listener
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                // user signed in
+                if (user != null) {
+                    Toast.makeText(LoginActivity.this, "로그인에 성공했습니다.", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+
+                    finish();
+
+                }
+                // User signed out
+                else {
+
+                }
+            }
+        };
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.email_login_button:
+                createAndLoginEmail();
+                break;
+        }
+    }
+
+    // 이메일 회원가입 및 로그인
+    private void createAndLoginEmail() {
+        auth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "createAndLoginEmail - successful");
+                            Toast.makeText(LoginActivity.this, "회원가입 성공", Toast.LENGTH_SHORT).show();
+                        }
+                        // 회원가입 에러 1. 비밀번호가 6자리 이상 입력 안된 경우
+                        else if (password.getText().toString().length() < 6){
+                            Log.d(TAG, "createAndLoginEmail - 비밀번호 6자리 이상 입력 안됨");
+                            Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                        // 이미 아이디가 있는 경우 에러 발생 X. 로그인 코드로 넘어감
+                        else {
+                            Log.d(TAG, "createAndLoginEmail - 아이디 이미 존재");
+                            signinEmail();
+                        }
+                    }
+                });
+    }
+
+    // 로그인 메소드
+    private void signinEmail() {
+        auth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "signinEmail - successful");
+                        }
+                        else {
+                            Log.d(TAG, "signinEmail - 비밀번호 틀림");
+                            Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
